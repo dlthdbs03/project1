652d15771c660f59d86760978528d4cc563f5ad8
dlthdbs03 <dlsth13@gmail.com>
2024-10-04 03:08:55 +0900

    Initial Commit

---
+++ project1.py
@@ -0,0 +1,14 @@
+num1 = int(input("첫 번째 숫자를 입력해주세요: "))
+op = input("연산을 입력해주세요: ")
+num2 = int(input("두 번째 숫자를 입력해주세요: "))
+
+result = 0
+
+if op == "+":
+    result = num1 + num2
+elif op == "-":
+    result = num1 - num2
+else:
+    print("잘못 입력했습니다.")
+
+print(result)
